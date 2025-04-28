7c6322ad6b8f847c4029fcca80e0b0a67ee36061
AveryV03 <sockmonkey0332@gmail.com>
2025-04-28 18:21:59 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-if ('ServiceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js')
             .then(reg => console.log('Service Worker registered!', reg))
